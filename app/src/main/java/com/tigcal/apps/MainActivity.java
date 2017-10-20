9baaf0ac5553ed9b1b801fae432553c0ca523a35
Jomar Tigcal <jomar@tigcal.com>
2017-10-20 22:28:31 +0800

    Adding Firebase and updating Gradle
    Signed-off-by: Jomar Tigcal <jomar@tigcal.com>

--- app/src/main/java/com/tigcal/apps/MainActivity.java
+++ app/src/main/java/com/tigcal/apps/MainActivity.java
@@ -1,7 +1,7 @@
 package com.tigcal.apps;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
