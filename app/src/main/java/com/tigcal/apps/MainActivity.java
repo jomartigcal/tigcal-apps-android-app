5f679ebcd5e3cdc6f271ad4041ce8c19fff16585
Jomar Tigcal <jomar@tigcal.com>
2017-11-29 15:54:57 -0800

    Adding click event on bottom navigation that will update the text above
    Signed-off-by: Jomar Tigcal <jomar@tigcal.com>

--- app/src/main/java/com/tigcal/apps/MainActivity.java
+++ app/src/main/java/com/tigcal/apps/MainActivity.java
@@ -1,13 +1,42 @@
 package com.tigcal.apps;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.text_view);
+
+        final BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_android:
+                        textView.setText(getString(R.string.menu_android));
+                        return true;
+                    case R.id.action_assistant:
+                        textView.setText(getString(R.string.menu_assistant));
+                        return true;
+                    case R.id.action_chrome:
+                        textView.setText(getString(R.string.menu_chrome));
+                        return true;
+                }
+
+                return false;
+            }
+        });
+
     }
 }
