54638b13f6a048e47f74e2fdebaca8510173a636
Jomar Tigcal <jomar@tigcal.com>
2017-12-09 07:30:27 -0800

    Changing toolbar title depending on selected category from the bottom navigation (instead of changing text view content)
    Signed-off-by: Jomar Tigcal <jomar@tigcal.com>

--- app/src/main/java/com/tigcal/apps/MainActivity.java
+++ app/src/main/java/com/tigcal/apps/MainActivity.java
@@ -5,18 +5,13 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        textView = findViewById(R.id.text_view);
 
         final BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -24,13 +19,13 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.action_android:
-                        textView.setText(getString(R.string.menu_android));
+                        setTitle(getString(R.string.menu_android));
                         return true;
                     case R.id.action_assistant:
-                        textView.setText(getString(R.string.menu_assistant));
+                        setTitle(getString(R.string.menu_assistant));
                         return true;
                     case R.id.action_chrome:
-                        textView.setText(getString(R.string.menu_chrome));
+                        setTitle(getString(R.string.menu_chrome));
                         return true;
                 }
 
@@ -38,5 +33,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        bottomNavigationView.setSelectedItemId(R.id.action_android);
     }
 }
