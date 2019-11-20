c1cc3dc93659b23c3e973770ef2bdcac5a20091c
Jomar Tigcal <jomar@tigcal.com>
2019-11-19 21:17:49 -0800

    Migrating LaunchScreenActivity to Kotlin

---
+++ app/src/main/kotlin/com/tigcal/apps/LaunchScreenActivity.kt
@@ -0,0 +1,15 @@
+package com.tigcal.apps
+
+import android.content.Intent
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+
+class LaunchScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
