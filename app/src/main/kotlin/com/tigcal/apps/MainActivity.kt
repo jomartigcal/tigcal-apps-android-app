72d451d4c7f33d810cf324e58af1dae008365dbc
Jomar Tigcal <jomar@tigcal.com>
2019-12-08 16:50:32 -0800

    Fixing Send Feedback not passing in the subject and text

--- app/src/main/kotlin/com/tigcal/apps/MainActivity.kt
+++ app/src/main/kotlin/com/tigcal/apps/MainActivity.kt
@@ -192,7 +192,8 @@ class MainActivity : AppCompatActivity() {
                 .append(")")
 
         val intent = Intent(Intent.ACTION_SENDTO)
-        intent.data = Uri.parse("mailto:info@sweldongpinoy.com")
+        intent.data = Uri.parse("mailto:");
+        intent.putExtra(Intent.EXTRA_EMAIL,  arrayOf("info@sweldongpinoy.com"))
         intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.send_feedback_subject, getString(R.string.app_name)))
         intent.putExtra(Intent.EXTRA_TEXT, deviceInfoBuilder.toString())
         startActivity(Intent.createChooser(intent, getString(R.string.send_feedback_header)))
